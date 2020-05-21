bf239f3f2af6ae601f025bc5132c2480ee2d6e32
Stanwang1210 <ch995308@gmail.com>
2020-05-21 13:34:20 +0800

    'upload'

--- pbs.sh
+++ pbs.sh
@@ -3,7 +3,7 @@
 #PBS -l select=1:ncpus=8:ngpus=1
 #PBS -q v100
 #PBS -o ./b07701209/HW7/pbs_output.o
-#PBS -e ./b07701209/HW7/pbs_error.e
+#PBS -e ./b07701209/HW7/pbs_error
 #PBS -m ea
 #PBS -M b07701209@ntu.edu.tw
 
@@ -17,7 +17,7 @@ cd ./HW7
 # python3 src/hw7_from_scratch.py
 # python3 src/hw7_test.py ./food-11 ./predict.csv
 # bash hw7_test.sh ./food-11 ./predict_2.csv
-bash HW7_Kaggle_Liu.py 
+bash python3 HW7_Kaggle_Liu.py 
 
 # cd ./hw9
 # python3 src/hw9_unsupervised.py
